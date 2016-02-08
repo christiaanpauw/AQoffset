4b3a9bf1fc1c2ca416991623b3a4c2e9685ccb08
christiaanpauw <c.j.pauw@gmail.com>
2016-02-08 21:28:57 +0200

    meer boodskappe

--- R/pointifyCensus.R
+++ R/pointifyCensus.R
@@ -4,7 +4,7 @@
 #' appropriate number of random points and return a SpatialPointsDataFrame
 #' @param spdf A SpatialPolygonsDataFrame
 #' @param dropnames A character vector of column names that should not be included
-#' @param verbode Do you want messages
+#' @param verbose Logical. Do you want messages?
 #' @details The numeric columns are counts of responses to categorical questions in the census
 #' e.g. Which energy carrier do you use for heating.
 #' 
@@ -13,20 +13,21 @@
 pointifyCensus <- function(spdf, 
                            dropnames=c("ID", "OBJECTID", "Geometry_s", "GAVPrimar0", "Geometry_1", 
                                        "SP_CODE","SP_Code", "MP_CODE", "MP_Code" ,"MN_CODE", "MN_MDB_C", "DC_MN_C",
-                                       "Shape_Leng", "Shape_Area", "fakeData", "GAVPrimary", "Total")){
+                                       "Shape_Leng", "Shape_Area", "fakeData", "GAVPrimary", "Total"),
+                           verbose = TRUE){
   dropidx <- na.omit(match(dropnames, names(spdf)))
-  if (verbose == TRUE) message(dim(spdf@data))
+  if (verbose == TRUE) message(paste(dim(spdf@data), collapse = " by "))
   if (verbose == TRUE) message("names : ", paste(names(spdf), " "))
 
   if (length(dropidx) > 0) spdf <- spdf[,-dropidx]
   numidx <- grep("numeric|integer", sapply(spdf@data, class ))
-  if (verbose == TRUE) message(paste(numidx, " "))
+  if (verbose == TRUE) message("numidx: " ,paste(numidx, " "), "\nEk vat dus ", paste(names(spdf)[numidx], " "))
   if (length(numidx) < 1) stop("There must be some numeric columns")
   # VERVANG INF MET NA
   spdf <- spdf[,numidx]
   if (length(spdf@data[spdf@data == Inf]) > 0) spdf@data[spdf@data == Inf] <- 0
   if (length(spdf@data[is.na(spdf@data)]) > 0) spdf@data[is.na(spdf@data)] <- 0
-  if (verbose == TRUE) message(dim(spdf@data))
+  if (verbose == TRUE) message(paste(dim(spdf@data), collapse = " by "))
   if (verbose == TRUE) message(paste(sapply(spdf@data, max, na.rm = TRUE), " "))
   res.list <- lapply(1:length(spdf), function(i){
     SpatialPointsDataFrame(coords = spsample(x = spdf[i, ], n = sum(spdf@data[i,], na.rm = TRUE), type = "random"),
@@ -36,5 +37,6 @@ pointifyCensus <- function(spdf,
                            bbox = NULL
     )
   })
+  if (verbose == TRUE) message("res.list gemaak")
   res <- do.call("rbind.SpatialPointsDataFrame", res.list)
 }
