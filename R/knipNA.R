fd8dab12d3458bea9baed0a3c54bab93ee5c9be6
christiaanpauw <c.j.pauw@gmail.com>
2016-02-08 21:28:04 +0200

    enklele klein veranderinge

--- R/knipNA.R
+++ R/knipNA.R
@@ -5,7 +5,7 @@
 
 
 knipNA <- function(r, out = c("extent", "raster")[1]){
-if (attr(class(s), "package") != "raster") stop("r should be a raster")
+if (attr(class(r), "package") != "raster") stop("r should be a raster")
 if (length(out) != 1) stop("out must be either 'extent' or 'raster' \n it's current length is ", length(out))
 if (is.na(match(out,  c("extent", "raster")))) stop(paste("\nout must be either 'extent' or 'raster'\n it is currently " , out, sep=""))
 
@@ -17,16 +17,16 @@ colNotNA <- which(colSums(r1NaM) != nrow(r))
 rowNotNA <- which(rowSums(r1NaM) != ncol(r))
 
 #Find the extent of the new raster by using the first and last columns and rows that are not completely filled by NAs. 
-
+if (length(rowNotNA) > 0 & length(colNotNA) > 0){
 r3Extent <- extent(r, rowNotNA[1], rowNotNA[length(rowNotNA)],
    colNotNA[1], colNotNA[length(colNotNA)])
+} else { r3Extent <- extent(r) }
 if (out == "extent"){
 	return(r3Extent )
 	} else {
 		r3 <- crop(r, r3Extent)
 		return(r3)
 	}
-
 
 #Plot the rasters for comparison.
 }
