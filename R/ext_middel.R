c05378805db3a091aeef483f445c4471d04b4d91
christiaanpauw <c.j.pauw@gmail.com>
2016-03-16 10:38:44 +0200

    Knip Eskom tot selfde groote as Kwaza

--- R/ext_middel.R
+++ R/ext_middel.R
@@ -1,8 +1,8 @@
 # maak 'n 4km raster van 'n middelpunt van 'n extent objek
 
-ext_middel <- function(ex, grdsize = 4, diam = 12, crs = "+proj=utm + south + zone=35 ellps=WGS84"){
-  xmid <- mean(extent(hh_annual)[1:2])
-  ymid <- mean(extent(hh_annual)[3:4])
+ext_middel <- function(r, grdsize = 4, diam = 12, crs = "+proj=utm + south + zone=35 ellps=WGS84"){
+  xmid <- mean(extent(r)[1:2])
+  ymid <- mean(extent(r)[3:4])
   xmin <- xmid - diam/2
   xmax <- xmid + diam/2
   ymin <- ymid - diam/2
