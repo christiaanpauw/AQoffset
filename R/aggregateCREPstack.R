f05da2ebbc8fc9f3d8f2be7d6d80943fd49cfab4
christiaanpauw <c.j.pauw@gmail.com>
2016-07-29 11:17:38 +0200

    sit "som" funksie in.

--- R/aggregateCREPstack.R
+++ R/aggregateCREPstack.R
@@ -4,18 +4,23 @@
 
 aggregateCREPstack <- function(r){
   # summarise the number of cases across all days
+  
+  som <- function(x) {sum(x, na.rm = TRUE)}
+
   r <- lapply(X = r, FUN = function(bOutcome) {
-    return(calc(x = bOutcome, fun = sum))
+    return(calc(x = bOutcome, fun = som))
   })
   r <- stack(r)
   
+  names(r) <- gsub("mortality", "MORT", names(r))
   names(r) <- gsub("among.", "", names(r))
   names(r) <- gsub("espiratory", "esp.", names(r))
-  names(r) <- gsub("hospital.admissions", "hosp.admin", names(r))
+  names(r) <- gsub("hospital.admissions", "hosp.ad", names(r))
   names(r) <- gsub("Cases_", "", names(r))
   names(r) <- gsub("due.to.", "", names(r))
-  names(r) <- gsub("Cardiovascular|Cardiovascular", "CV", names(r))
+  names(r) <- gsub("Cardiovascular|cardiovascular", "CV", names(r))
   names(r) <- gsub("all.ages", "all", names(r))
+  r[is.na(r)] <- 0.000001
   r
 }
 
