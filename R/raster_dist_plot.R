9332a41097105f969066ec36ec97329944ececc9
christiaanpauw <c.j.pauw@gmail.com>
2016-07-29 11:18:15 +0200

    nog @param

--- R/raster_dist_plot.R
+++ R/raster_dist_plot.R
@@ -11,7 +11,9 @@
 #' @param scales A list with components x (columns) and     y (rows). Each of 
 #' these components must be a numeric vector of length 2 defining the range for each marginal plot.
 #' Default contains a logical that prevents drawing marginal graphics
-#' @param ... Arguments to be passed to/from other methods 
+#' @param ... Arguments to be passed to raster_dist_sum 
+#' @param meanonly Logical Return only the mean
+#' @param tab_out Logical Write out a table
 #' @export
 
 raster_dist_plot <- function(ss, 
@@ -20,7 +22,10 @@ raster_dist_plot <- function(ss,
                              mn = "", 
                              sb ="",
                              th = BuRdTheme,
-                             scales=list(draw=FALSE),
+                             scales=list(draw=FALSE), 
+                             meanonly = FALSE, 
+                             tab_out = FALSE,
+                             debug = FALSE, beginnul = FALSE,
                              ...){
   if (!require(rasterVis)){
     message("Ek probeer raserVis installeer")
@@ -29,7 +34,21 @@ raster_dist_plot <- function(ss,
   }
   if (is.null(multi)){
     x <- raster_dist_sum(s = ss, ... )
-    levelplot(x, par.settings = BuRdTheme, main = mn, sub =sb, scales = scales, ...)
+    if (tab_out){
+      d <- data.frame(matrix(cellStats(x, mean), ncol = 1))
+      rownames(d) <- names(x)
+      colnames(d) <- "mean"
+      return(d)
+    }
+    if (meanonly){
+      if (length(grep("mean", names(x))) > 0){
+        levelplot(x[[grep("mean", names(x))]], par.settings = BuRdTheme, main = mn, sub =sb, scales = scales, margin = list(draw = FALSE), ...)
+      } else {
+        levelplot(x, par.settings = BuRdTheme, main = mn, sub =sb, scales = scales, ...)
+      }
+    } else {
+      levelplot(x, par.settings = BuRdTheme, main = mn, sub =sb, scales = scales, ...)
+    }
   } else {
     if (!is.character(multi)) stop("multi must be NULL or a character vector")
     ev <- new.env()
@@ -47,6 +66,7 @@ raster_dist_plot <- function(ss,
     res = stack(res2)
     lo = c(6,length(multi))
     mmv <- min(minValue(res))
+    if (beginnul) mmv <- 0
     message(mmv)
     if (mmv > 0){
       att <- seq(0, max(maxValue(res)), length = 20)
@@ -54,7 +74,21 @@ raster_dist_plot <- function(ss,
       att <- seq(mmv, max(maxValue(res)), length = 20)
     }
     
-    levelplot(res, par.settings = th, layout = lo, main = mn, sub =sb, scales =scales, at = att)
-  }
-  
+    if (tab_out){
+      rn <- unique(gsub("([[:alnum:]]+)\\.([[:print:]]+)", "\\2", names(res)))
+      cln <- unique(gsub("([[:alnum:]]+)\\.([[:print:]]+)", "\\1", names(res)))
+      m <- matrix(cellStats(res, mean), ncol = 2, dimnames = list(rn, cln))
+      d <- data.frame(m)
+      rownames(d) <- rn
+      colnames(d) <- cln
+      return(d)
+    }
+    
+    if (meanonly){
+      res <- res[[grep("mean", names(res))]]
+      levelplot(res, par.settings = th, main = mn, sub =sb, scales = scales)
+    } else {
+      levelplot(res, par.settings = th, layout = lo, main = mn, sub =sb, scales =scales, at = att)
+    }
+    }
 }
